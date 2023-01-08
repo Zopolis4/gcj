150da872d5300396df37f0c0e2ade5e00b400d6a
Iain Sandoe <iain@sandoe.co.uk>
2023-01-08 20:25:58 +0000

    libjava: Do not use the Darwin FDE shim except on Darwin10.
    The libjava implementation uses a workaround for a bug in the unwinder on
    Darwin10, this workaround is not needed (or available) on later editions of
    the compiler.
    
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>
    
    libjava/ChangeLog:
    
    	* include/posix.h: Only use the Darwin FDE workaround on Darwin10.

--- libjava/include/posix.h
+++ libjava/include/posix.h
@@ -51,7 +51,8 @@ details.  */
 #define _Jv_platform_solib_suffix ".so"
 #endif
 
-#if __MACH__ && (__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ >= 1060)
+#if __APPLE__ && (__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ >= 1060) \
+   && (__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ < 1070)
 #  undef _Unwind_FindEnclosingFunction
 #  define _Unwind_FindEnclosingFunction(PC) _darwin10_Unwind_FindEnclosingFunction(PC)
 #endif
